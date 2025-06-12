356b5640ae864a1840eabe774d3f895401f0676a
Khemraj Saud <khemrajsaud56@gmail.com>
2025-06-12 22:23:09 +0545

    change

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,32 +1,67 @@
 
+// import CategoryProducts from "@/components/CategoryProducts";
+// import Container from "@/components/Container";
+// import { Title } from "@/components/text";
+// import { getCategories } from "@/sanity/lib/queries";
+// import React from "react";
+
+
+
+// const CategoryPage = async ({
+//   params,
+// }: {
+//    params: { slug: string };
+// }) => {
+
+//   const categories = await getCategories();
+//   console.log(categories);
+  
+//   const {slug} = params;
+//   return (
+//     <div className="py-10">
+//       <Container>
+//         <Title>Product by Categories:{""}
+//           <span className="font-bold text-green-600 capitalize tracking-wide">
+//             {slug && slug}
+//           </span>
+//         </Title>
+//         <div className="mt-5 items-center">
+//         <CategoryProducts categories={categories}  slug={slug} />
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default CategoryPage;
+
 import CategoryProducts from "@/components/CategoryProducts";
 import Container from "@/components/Container";
 import { Title } from "@/components/text";
 import { getCategories } from "@/sanity/lib/queries";
 import React from "react";
 
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
 
-
-const CategoryPage = async ({
-  params,
-}: {
-   params: { slug: string };
-}) => {
-
+const CategoryPage = async ({ params }: PageProps) => {
   const categories = await getCategories();
-  console.log(categories);
-  
-  const {slug} = params;
+  const { slug } = params;
+
   return (
     <div className="py-10">
       <Container>
-        <Title>Product by Categories:{""}
+        <Title>
+          Product by Categories:{" "}
           <span className="font-bold text-green-600 capitalize tracking-wide">
-            {slug && slug}
+            {slug}
           </span>
         </Title>
         <div className="mt-5 items-center">
-        <CategoryProducts categories={categories}  slug={slug} />
+          <CategoryProducts categories={categories} slug={slug} />
         </div>
       </Container>
     </div>
@@ -34,6 +69,7 @@ const CategoryPage = async ({
 };
 
 export default CategoryPage;
+
 
 
 
